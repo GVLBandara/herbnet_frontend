3e9316cdcf8738bb865cdca05725679ff7b2d6f0
GotabayaV <gotabaya.vimukthi@gmail.com>
2024-02-03 04:25:46 +0530

    Search box design

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,28 @@
 import React, {useState} from 'react'
 import {IoIosArrowDown} from "react-icons/io";
-import { IoSearchSharp } from "react-icons/io5";
-import { BsThreeDots } from "react-icons/bs";
+import {IoSearchSharp} from "react-icons/io5";
+import {BsThreeDots} from "react-icons/bs";
 
 const Home = () => {
-	const [part, setPart] = useState("Roots")
-	const [state, setState] = useState("Fresh")
+	const [part, setPart] = useState("")
+	const [state, setState] = useState("")
+	const [prompt, setPrompt] = useState("")
 
+	const handlePart = (e) => {
+		setPart(e.target.value)
+	}
+
+	const handleState = (e) => {
+		setState(e.target.value)
+	}
+
+	const handlePrompt = (e) => {
+		setPrompt(e.target.value)
+	}
+
+	const handleSearch = () => {
+		console.log(prompt, part, state)
+	}
 
 	return (
 		<div className={`flex justify-around items-center h-[85vh]`}>
@@ -15,22 +31,59 @@ const Home = () => {
 					Your Marketplace for Buying & Selling Quality Herbal Tree Parts!
 				</h1>
 				<div className={`bg-white h-20 flex justify-between	 items-center mx-12 p-1 rounded-[13px]`}>
-					<input className={`pl-4 h-full w-[35vw] text-4xl font-medium text-[#7b7b7b] placeholder:text-[#7b7b7b]`} type={"text"} placeholder={"Search for Products"}/>
+					<input
+						className={`pl-4 h-full w-[35vw] text-4xl font-medium text-[#7b7b7b] focus:outline-none placeholder:text-[#7b7b7b]`}
+						type={"text"} placeholder={"Search for Products"}
+						onChange={handlePrompt}
+						onKeyDown={event => {
+							if (event.key === 'Enter') handleSearch()
+						}}
+					/>
+
 					<div className={`flex gap-1 h-full`}>
+
 						<div
 							className={`h-full text-3xl text-[#7b7b7b] font-medium bg-gray-200 rounded-[8px] flex justify-around items-center gap-1 px-2`}>
-							<h1>Roots</h1>
-							<IoIosArrowDown/>
+							<div className="relative">
+								<select
+									className={`bg-gray-200 appearance-none pl-1 pr-8 focus:outline-none`}
+									onChange={handlePart}>
+									<option value="">All Parts</option>
+									<option value="Roots">Roots</option>
+									<option value="Leaves">Leaves</option>
+									<option value="Fruits">Fruits</option>
+									<option value="Flowers">Flowers</option>
+								</select>
+								<div className={`absolute inset-y-0 right-0 flex items-center pointer-events-none`}>
+									<IoIosArrowDown className={`fill-current text-3xl text-gray-500`}/>
+								</div>
+							</div>
 						</div>
+
 						<div
 							className={`h-full text-3xl text-[#7b7b7b] font-medium bg-gray-200 rounded-[8px] flex justify-around items-center gap-1 px-2`}>
-							<h1>Fresh</h1>
-							<IoIosArrowDown/>
+							<div className="relative">
+								<select
+									className={`bg-gray-200 appearance-none pl-1 pr-8 focus:outline-none`}
+									onChange={handleState}>
+									<option value="">All States</option>
+									<option value="Fresh">Fresh</option>
+									<option value="Dried">Dried</option>
+									<option value="Juiced">Juiced</option>
+									<option value="Powdered">Powdered</option>
+								</select>
+								<div className={`absolute inset-y-0 right-0 flex items-center pointer-events-none`}>
+									<IoIosArrowDown className={`fill-current text-3xl text-gray-500`}/>
+								</div>
+							</div>
 						</div>
+
 						<div
-							className={`h-full text-5xl text-[#7b7b7b] bg-gray-200 rounded-[8px] flex justify-around items-center px-4`}>
+							className={`h-full text-5xl text-[#7b7b7b] bg-gray-200 rounded-[8px] flex justify-around items-center px-4 cursor-pointer`}
+							onClick={handleSearch}>
 							<IoSearchSharp/>
 						</div>
+
 					</div>
 				</div>
 				<div className={`flex justify-center`}>
